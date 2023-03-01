1c77b00a91d541d8ddb4ff6c0ecf4c28deb21fba
lhevondian <115144884+lhevondian@users.noreply.github.com>
2023-03-02 00:39:25 +0200

    implemented identity

---
+++ src/CatTheoryDotnetCore/Identity.cs
@@ -0,0 +1,10 @@
+﻿namespace CatTheoryDotnetCore
+{
+    public static class Identity
+    {
+        public static Func<T, T> GetIdentity<T>(this T category)
+        {
+            return x => x;
+        }
+    }
+}

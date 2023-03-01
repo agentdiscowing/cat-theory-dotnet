71bd2459c586202858d32e1510b5bcf20c6d1be9
agentdiscowing <agentdiscowing@gmail.com>
2023-03-02 01:39:47 +0200

    Create SingletonSet.cs
    added unit

---
+++ src/CatTheoryDotnetCore/SingletonSet.cs
@@ -0,0 +1,8 @@
+﻿namespace CatTheoryDotnetCore
+{
+    public static class SingletonSet
+    {
+        // maps any type to the singleton set void (set of void can only have one object - void)
+        public static void Unit<T>(T input) { }
+    }
+}

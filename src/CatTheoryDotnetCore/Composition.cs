5e9ed5fb548b9698c583dbda005aff59bf29fec4
lhevondian <115144884+lhevondian@users.noreply.github.com>
2023-03-02 00:39:17 +0200

    implemented composition

---
+++ src/CatTheoryDotnetCore/Composition.cs
@@ -0,0 +1,11 @@
+﻿namespace CatTheoryDotnetCore
+{
+    public static class Composition
+    {
+        public static Func<TInput, TOutput> Compose<TInput, TOutput, TLink>(this Func<TLink, TOutput> outer, Func<TInput, TLink> inner)
+        {
+            return (TInput x) => outer(inner(x));
+        }
+
+    }
+}
